d8dd36e6f3e32f4e9e92a2c997aa2319df878f5a
Grant West <grant.west@angel.com>
2024-02-23 08:20:00 -0600

    initial

---
+++ lib/httpc_test.ex
@@ -0,0 +1,18 @@
+defmodule HttpcTest do
+  @moduledoc """
+  Documentation for `HttpcTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HttpcTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/httpc_test_test.exs
@@ -0,0 +1,23 @@
+defmodule HttpcTestTest do
+  use ExUnit.Case
+  doctest HttpcTest
+
+  test "check ssl" do
+    [
+      "https://expired.badssl.com/",
+      "https://wrong.host.badssl.com/",
+      "https://self-signed.badssl.com/",
+      "https://untrusted-root.badssl.com/",
+      "https://revoked.badssl.com/",
+      "https://no-common-name.badssl.com/",
+      "https://no-subject.badssl.com/",
+      "https://incomplete-chain.badssl.com/",
+      "https://dh480.badssl.com/"
+    ]
+    |> Enum.each(fn url ->
+      assert_raise Tesla.Error, fn ->
+        Tesla.get!(url)
+      end
+    end)
+  end
+end
